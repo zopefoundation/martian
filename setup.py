a9130923c14db120b486ff12c09667dc1e4dbaf7
Philipp von Weitershausen <philipp@weitershausen.de>
2007-10-30 19:12:12 +0000

    remove dev marker for release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ long_description = (
 
 setup(
     name='martian',
-    version='0.9.1dev',
+    version='0.9.1',
     author='Grok project',
     author_email='grok-dev@zope.org',
     description="""\
