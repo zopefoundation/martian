6845b2b4217be5277bd13581d812391dd9666b28
Martijn Faassen <faassen@startifact.com>
2007-11-20 12:22:12 +0000

    Bump version number.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ long_description = (
 
 setup(
     name='martian',
-    version='0.9.2dev',
+    version='0.9.3dev',
     author='Grok project',
     author_email='grok-dev@zope.org',
     description="""\
