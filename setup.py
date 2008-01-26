63c1f434970d7a935c01ad0750766f1a8c1b0d74
Martijn Faassen <faassen@startifact.com>
2008-01-26 22:18:02 +0000

    Prepare for release.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ long_description = (
 
 setup(
     name='martian',
-    version='0.9.3dev',
+    version='0.9.3',
     author='Grok project',
     author_email='grok-dev@zope.org',
     description="""\
