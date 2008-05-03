de33a9ed6e14fe654503ed52980c4c18c2e94fdb
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2008-05-03 12:24:03 +0000

    Import and test MultipleTimeDirective.

--- src/martian/__init__.py
+++ src/martian/__init__.py
@@ -3,8 +3,9 @@ from martian.core import (
     grok_package, grok_module)
 from martian.components import GlobalGrokker, ClassGrokker, InstanceGrokker
 from martian.util import scan_for_classes
-from martian.directive import Directive, MarkerDirective, baseclass
+from martian.directive import Directive, MarkerDirective, MultipleTimesDirective
 from martian.directive import ONCE, MULTIPLE, DICT
 from martian.directive import CLASS, CLASS_OR_MODULE, MODULE
 from martian.directive import (
     validateText, validateInterface, validateInterfaceOrClass)
+from martian.directive import baseclass
