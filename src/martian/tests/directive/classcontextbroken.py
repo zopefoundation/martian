a5aa21369f61073ce324cfdac2133b751d5271a7
Philipp von Weitershausen <philipp@weitershausen.de>
2008-05-03 15:50:46 +0000

    Remove obsolete tests.

--- src/martian/tests/directive/classcontextbroken.py
+++
@@ -1,5 +0,0 @@
-from martian.directive import SingleTextDirective, ClassDirectiveContext
-
-bar = SingleTextDirective('grok.bar', ClassDirectiveContext())
-
-bar('hello world') # this won't work as not class context
