a742c9e74473c6ca9572665615f2d1149594f80a
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2009-10-07 17:53:14 +0000

    Back to development: 0.11.2dev

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (
 
 setup(
     name='martian',
-    version = '0.11.1',
+    version = '0.11.2dev',
     author='Grok project',
     author_email='grok-dev@zope.org',
     description="""\
