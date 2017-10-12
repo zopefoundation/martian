00071323eee52ae8aea7f633d02cdbc84cb7576e
Michael Howitz <mh@gocept.com>
2017-10-12 08:15:44 +0200

    Rename .txt files to match content and add badges. (#5)
    * Rename .txt files to match content and add badges.

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 long_description = (
-    read('README.txt')
+    read('README.rst')
     + '\n' +
-    read('src', 'martian', 'README.txt')
+    read('src', 'martian', 'README.rst')
     + '\n' +
-    read('CHANGES.txt')
+    read('CHANGES.rst')
     + '\n' +
     'Download\n'
     '********\n'
