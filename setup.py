0602a84509b85beb5da390d53d3c6d956adf2349
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-01-25 09:14:20 +0100

    Back to development: 1.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = (
 
 setup(
     name='martian',
-    version='1.1',
+    version='1.2.dev0',
     url='https://github.com/zopefoundation/martian',
     author='Grok project',
     author_email='grok-dev@zope.org',
