1aaf5ed7a173653a2639a9c7198d6e3c2db59f58
Michael Howitz <mh@gocept.com>
2022-12-30 10:19:04 +0100

    Little clean up.

--- src/martian/util.py
+++ src/martian/util.py
@@ -24,8 +24,7 @@ from martian.error import GrokError
 
 
 def not_unicode_or_ascii(value):
-    # python3 compatibility
-    if isinstance(value, str):  # NOQA
+    if isinstance(value, str):
         return False
     if not isinstance(value, str):
         return True
